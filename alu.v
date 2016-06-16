c81baab6230ea235518aaf970d015f530f1bc5e6
MrwanBaghdad <mrwanbaghdad76@gmail.com>
2016-06-16 12:40:10 +0200

    started alu

---
+++ alu.v
@@ -0,0 +1,8 @@
+module alu(in1,in2,aluop,out);
+input [31:0]in1,in2;
+output[31:0]out;
+input []aluop;
+
+case(aluop)
+
+end module;
---
+++ processor.v
@@ -0,0 +1,8 @@
+'include clock.v'
+'include clock2.v'
+'include registerFile.v'
+'include mux2.v'
+'include mux3.v'
+'include mux4.v'
+'include memory.v'
+
